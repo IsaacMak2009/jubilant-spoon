978d872cd6d8f2ba19244e7de5ab2097e2130018
IsaacMak2009 <chonheimak@gmail.com>
2024-01-16 18:00:59 +0800

    first submit

--- core/chassis.py
+++ core/chassis.py
@@ -46,59 +46,31 @@ class PIDController(Controller):
 
 
 class Chassis:
-    def __init__(self, topic_name: str = "/cmd_vel", imu_topic: Optional[str] = "/imu"):
-        self.imu_data: Optional[Imu] = None
+    def __init__(self, topic_name: str = "/cmd_vel", odom_topic: Optional[str] = "/odom"):
         self.isready = False
 
         self.topic_name = topic_name
-        self.imu_topic = imu_topic
-        self.use_imu = True
+        self.odom_topic = odom_topic
+        self.use_odom = False
+
+        self.odom_data: Optional[Odometry] = None
 
         logger.info("Initializing Chassis")
         self.publisher = rospy.Publisher(self.topic_name, Twist, queue_size=10)
 
-        # logger.info("Initializing Odometry...")
-        # if self.odom_topic:
-        #     logger.debug("Subscribing to odometry topic...")
-        #     self.odom_sub = rospy.Subscriber(self.odom_topic, Odometry, self.odom_callback)
-        #     try:
-        #         rospy.wait_for_message(self.odom_topic, Odometry, timeout=3)
-        #     except rospy.ROSException:
-        #         logger.warning("Timeout occurred while waiting for odometry message.")
-        #         self.use_odom = False
-        # else:
-        #     self.use_odom = False
-        #
-        # if not self.use_odom:
-        #     logger.warning(f"odom ({self.odom_topic}) is not available")
-
-        logger.info("Initializing Imu...")
-        if self.imu_topic:
-            logger.debug("Subscribing to imu topic...")
-            self.imu_sub = rospy.Subscriber(self.imu_topic, Imu, self.imu_callback)
+        logger.info("Initializing Odometry...")
+        if self.odom_topic:
+            logger.debug("Subscribing to odometry topic...")
+            self.odom_sub = rospy.Subscriber(self.odom_topic, Odometry, self.odom_callback)
             try:
-                rospy.wait_for_message(self.imu_topic, Imu, timeout=3)
+                rospy.wait_for_message(self.odom_topic, Odometry, timeout=3)
             except rospy.ROSException:
-                logger.warning("Timeout occurred while waiting for imu message.")
-                self.use_imu = False
+                logger.warning("Timeout occurred while waiting for odometry message.")
+                logger.warning("Please make sure odometry is available")
+                self.use_odom = False
         else:
-            self.use_imu = False
+            self.use_odom = False
 
-        if not self.use_imu:
-            logger.warning(f"imu ({self.imu_topic}) is not available")
+    def odom_callback(self, msg: Odometry):
+        self.odom_data = msg
 
-        self.isready = True
-        logger.success("Chassis is ready!")
-
-    def imu_callback(self, msg: Imu):
-        self.imu_data = msg
-
-    def turn(self, deg: float):
-        quaternion = (
-            self.imu_data.orientation.x,
-            self.imu_data.orientation.y,
-            self.imu_data.orientation.z,
-            self.imu_data.orientation.w,
-        )
-
-        pitch, yaw, roll = euler_from_quaternion(quaternion)
